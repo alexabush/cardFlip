59bf7b228c7b0d547e10651fe1eb33666368d109
Alex Bush <alexabush7@gmail.com>
2018-01-27 21:17:16 -0800

    successfully prevented more than two cards from being flipped at a time

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const gameState = {
   lastCardClicked: undefined,
   isGameWon: false,
   numberCardsFlipped: 0,
-  numPairsMatched: 0
+  numPairsMatched: 0,
+  preventFlip: false
 };
 
 
@@ -17,47 +18,29 @@ newGameBtn.addEventListener('click', function(e) {
 });
 
 main.addEventListener('click', function(e) {
-  // debugger;
-  if (e.target.classList.contains('card')) {
-    e.target.classList.toggle('hidden');
+  if (e.target.classList.contains('card') && !gameState.preventFlip) {
     gameState.totalNumClicks += 1;
     if (gameState.numberCardsFlipped === 0) {
+      e.target.classList.toggle('hidden');
       gameState.numberCardsFlipped = 1;
       gameState.lastCardClicked = e.target;
     } else if (gameState.numberCardsFlipped === 1) {
-      gameState.numberCardsFlipped = 2; //this is uneccesary
+      e.target.classList.toggle('hidden');
       if (e.target.innerText === gameState.lastCardClicked.innerText) {
         gameState.numPairsMatched += 1;
         checkForWin();
         gameState.numberCardsFlipped = 0;
-        gameState.lastCardClicked = undefined; //this should be uneccesary
       } else {
+        gameState.preventFlip = true;
         setTimeout(function() {
           e.target.classList.toggle('hidden');
           gameState.lastCardClicked.classList.toggle('hidden');
-          gameState.numberCardsFlipped = false;
+          gameState.numberCardsFlipped = 0;
           gameState.lastCardClicked = undefined;
+          gameState.preventFlip = false;
         }, 1000);
       }
-
     }
-
-    //   if (e.target.innerText === gameState.lastCardClicked.innerText) {
-    //     gameState.numPairsMatched += 1;
-    //     gameState.numberCardsFlipped = false;
-    //     checkForWin();
-    //   } else {
-    //     setTimeout(function() {
-    //       e.target.classList.toggle('hidden');
-    //       gameState.lastCardClicked.classList.toggle('hidden');
-    //       gameState.numberCardsFlipped = false;
-    //       gameState.lastCardClicked = undefined;
-    //     }, 1000);
-    //   }
-    // } else {
-    //   gameState.numberCardsFlipped = true;
-    //   gameState.lastCardClicked = e.target;
-    // }
   }
 });
 
